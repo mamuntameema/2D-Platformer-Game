b2d5ef8a0de67fbba374c3a3410f48bdb26a3eca
mamuntameema <143912915+mamuntameema@users.noreply.github.com>
2023-09-24 14:28:20 -0400

    Added multiple levels, start screen, and end screen

--- 2D Platformer Game/Assets/Scripts/Finish.cs
+++ 2D Platformer Game/Assets/Scripts/Finish.cs
@@ -1,28 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
-    // Start is called before the first frame update
+    private bool levelCompleted = false;
     private void Start()
     {
         finishSound = GetComponent<AudioSource>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name =="Player")
+        if (collision.gameObject.name =="Player" && !levelCompleted)
         {
             finishSound.Play();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
             CompleteLevel();
         }
     }
 
     private void CompleteLevel()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
 }  
         
-}
+
